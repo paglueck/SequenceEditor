768652ecd74202f171ad393b5f395809772dd0c4
patrick <patrick@macbook-pro.fritz.box>
2017-01-19 10:54:53 +0100

    Initial

---
+++ src/SequenceViewer/Services.java
@@ -0,0 +1,24 @@
+package SequenceViewer;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.eclipse.uml2.uml.Interaction;
+import org.eclipse.uml2.uml.Message;
+
+/**
+ * The services class used by VSM.
+ */
+public class Services {
+    
+    /**
+    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
+    */
+    
+    
+    public String testService(EObject e) {
+    	//Message m = (Message) e;
+    	return "";
+    }
+    
+    
+}
